d65a7df83579d3c7a3cc52925c0dd55806abebf6
AztekAsylum <mateo.rodriguez85@gmail.com>
2023-09-28 17:46:14 -0600

    starter code

---
+++ server.js
@@ -0,0 +1,17 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+
+const PORT = 3001;
+
+// app.listen(PORT);
+// console.log(`API Server is listening on ${PORT} `);
+
+// // Connect to database
+// const db = mysql.createConnection({
+//   host: "127.0.0.1",
+//   // MySQL username,
+//   user: "root",
+//   // TODO: Add MySQL password
+//   password: "Brainscan93",
+//   database: "employee_db",
+// });
